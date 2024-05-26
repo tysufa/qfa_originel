e54e446202db3b1b56f7cea70d2ed02e6efe6066
tysufa <philemon.penot@gmail.com>
2024-05-26 22:48:50 +0200

    repl now show the evaluation of the ast

--- repl/repl.go
+++ repl/repl.go
@@ -3,10 +3,11 @@ package repl
 import (
 	"bufio"
 	"fmt"
-	"github.com/tysufa/qfa/token"
 	"os"
 
+	"github.com/tysufa/qfa/evaluator"
 	"github.com/tysufa/qfa/lexer"
+	"github.com/tysufa/qfa/parser"
 )
 
 func Run() {
@@ -16,10 +17,20 @@ func Run() {
 		input, _ := reader.ReadString('\n')
 
 		l := lexer.New(input)
-		tok := l.GetToken()
-		for tok.Type != token.EOF && tok.Type != token.NL {
-			fmt.Printf("{%v : %v}\n", tok.Type, tok.Value)
-			tok = l.GetToken()
+		// tok := l.GetToken()
+		// for tok.Type != token.EOF && tok.Type != token.NL {
+		// 	fmt.Printf("{%v : %v}\n", tok.Type, tok.Value)
+		// 	tok = l.GetToken()
+		// }
+		p := parser.New(l)
+		stmts := p.GetStatements()
+		evaluated := evaluator.EvaluateStatements(stmts)
+		if evaluated != nil {
+			for _, ev := range evaluated {
+				if ev != nil {
+					fmt.Printf("%v\n", ev.Inspect())
+				}
+			}
 		}
 	}
 }
