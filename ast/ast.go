12559e79873a378777452bd166cae70c94bd6742
tysufa <philemon.penot@gmail.com>
2024-05-26 22:49:28 +0200

    added String method to Program so that it is a node as well

--- ast/ast.go
+++ ast/ast.go
@@ -14,6 +14,16 @@ type Program struct {
 	Statements []Statement
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, stmt := range p.Statements {
+		out.WriteString(stmt.String())
+	}
+
+	return out.String()
+}
+
 type Statement interface {
 	Node
 	TokenLiteral() string
