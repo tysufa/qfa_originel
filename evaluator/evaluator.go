41ddd7f1962c7f4e1aafc910935cbd5bf0583684
tysufa <philemon.penot@gmail.com>
2024-05-27 13:16:44 +0200

    added evaluation of prefix expressions and equality tests

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -18,11 +18,43 @@ func Evaluate(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return boolToBoolObject(node.Value)
+	case *ast.PrefixExpression:
+		return evaluatePrefix(node)
 	case *ast.InfixExpression:
-		return evaluteExression(node)
+		return evaluteInfixExpression(node)
 	}
 
 	return nil
+}
+
+func evaluatePrefix(node *ast.PrefixExpression) object.Object {
+	right := Evaluate(node.Right)
+	if node.Operator == "!" {
+		return evaluateBangOperatorExpression(right)
+	} else if node.Operator == "-" {
+		return evaluateMinusOperatorExpression(right)
+	} else {
+		return nil
+	}
+}
+
+func evaluateMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return nil
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evaluateBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	default:
+		return nil
+	}
 }
 
 func boolToBoolObject(b bool) *object.Boolean {
@@ -32,7 +64,7 @@ func boolToBoolObject(b bool) *object.Boolean {
 	return FALSE
 }
 
-func evaluteExression(node *ast.InfixExpression) object.Object {
+func evaluteInfixExpression(node *ast.InfixExpression) object.Object {
 	res := &object.Integer{}
 	rightInt, ok := Evaluate(node.Right).(*object.Integer)
 	if !ok {
@@ -54,6 +86,18 @@ func evaluteExression(node *ast.InfixExpression) object.Object {
 		return &object.Integer{Value: right * left}
 	case "/":
 		return &object.Integer{Value: right / left}
+	case "==":
+		return boolToBoolObject(right == left)
+	case "!=":
+		return boolToBoolObject(right != left)
+	case ">":
+		return boolToBoolObject(right > left)
+	case ">=":
+		return boolToBoolObject(right >= left)
+	case "<":
+		return boolToBoolObject(right < left)
+	case "<=":
+		return boolToBoolObject(right <= left)
 	}
 
 	return res
